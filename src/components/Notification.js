1e4bcc340410f67caa379856008bbe5eddd8e1aa
Aleksi Paavola <paavola.aleksi@gmail.com>
2022-03-02 11:41:26 +0200

    add part 5 exercises

---
+++ src/components/Notification.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Notification({ notification }) {
+  if (notification === null) {
+    return null;
+  }
+
+  return (
+    <div className={notification.type}>
+      {notification.message}
+    </div>
+  );
+}
+
+export default Notification;
